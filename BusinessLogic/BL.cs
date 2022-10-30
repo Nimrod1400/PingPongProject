b247e94070cb025ff0d0ecf1f1e40725e3aa1a45
Вадим Шамрай <shamray-95@bk.ru>
2022-10-30 12:41:34 +0700

    Добавьте файлы проекта.

---
+++ BusinessLogic/BL.cs
@@ -0,0 +1,45 @@
+﻿using Model;
+using System;
+
+namespace BusinessLogic
+{
+    public class BL
+    {
+        public Game Game { get; set; } = new Game();
+
+        public void ResetMatch()
+        {
+            Game.FirstSideScore = 0;
+            Game.SecondSideScore = 0;
+        }
+
+        public void ResetGame()
+        {
+            Game Match = new Game();
+        }
+
+        public void IncrementFirstSideScore()
+        {
+            Game.FirstSideScore++;
+
+            if (Game.FirstSideScore >= Game.MaxScore && 
+                Game.FirstSideScore - Game.SecondSideScore == 2)
+            {
+                Game.FirstSideMatchesScore++;
+                ResetGame();
+            }
+        }
+
+        public void IncrementSecondSideScore()
+        {
+            Game.SecondSideScore++;
+
+            if (Game.SecondSideScore >= Game.MaxScore &&
+                Game.SecondSideScore - Game.FirstSideScore == 2)
+            {
+                Game.SecondSideMatchesScore++;
+                ResetGame();
+            }
+        }
+    }
+}
---
+++ Model/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Model
+{
+    public class Game
+    {
+
+        public byte MaxScore { get; set; } = 11;
+        public byte WhoStartedGame { get; set; } // 1 (first side) or 2 (second side);
+                                                 // defines who was kicking off when first match in a game started
+        public byte WhoStartedMatch
+        {
+            get
+            {
+                int amountOfMatches = FirstSideMatchesScore + SecondSideMatchesScore;
+
+                if (amountOfMatches % 2 == 0)
+                    return WhoStartedGame;
+                else
+                    return InverseServe(WhoStartedGame);
+            }
+        } 
+        public byte WhoIsServing
+        {
+            get
+            {
+                byte servesAmount = (byte)(SecondSideScore + FirstSideScore + 1);
+
+                if (servesAmount % 4 == 0 || (servesAmount + 1) % 4 == 0)                
+                    return InverseServe(WhoStartedMatch);                
+                else                
+                    return WhoStartedMatch;                
+            }
+        } // defines who must be kicking off rn
+        public byte FirstSideScore { get; set; }
+        public byte SecondSideScore { get; set; }
+        public byte FirstSideMatchesScore { get; set; }
+        public byte SecondSideMatchesScore { get; set; }
+
+        private byte InverseServe(byte currentServe)
+        {
+            if (currentServe == 1) { return 2; }
+            else { return 1; }
+        }
+    }
+}
